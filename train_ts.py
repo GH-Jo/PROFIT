020b3be0aeb5b9280dc0063cbd88cd2d1eee7433
GH-Jo <lkj004124@gmail.com>
2021-01-12 12:58:55 +0900

    Fix argument reference

--- train_ts.py
+++ train_ts.py
@@ -332,7 +332,7 @@ if args.pg:
             best_candi=train_epochs(optimizer, args.warmup, args.ft_epoch, prefix + "_ft3")
             best_acc = max(best_acc, best_candi)
 
-            if args.unfreezing:
+            if args.unfreeze:
                 params = categorize_param(model, skip_list)
                 optimizer = get_optimizer(params, train_quant=True, train_weight=True, train_bnbias=True) 
                 best_candi=train_epochs(optimizer, args.warmup, args.ft_epoch, prefix + "_ft4")
